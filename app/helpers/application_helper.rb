a3d976820924ed5b89376769e564628cea849c77
Danil Pismenny <danil@brandymint.ru>
2017-01-29 14:41:18 +0300

    Логин в шапке

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+  def application_title
+    'OnlineTrade'
+  end
+
+  def destroy_button(resource)
+    last_resource = resource.is_a?(Array) ? resource.last : resource
+    return unless last_resource.persisted?
+    link_to 'Удалить',
+      url_for(resource),
+      method: :delete,
+      data: { confirm: 'Удалить?' },
+      class: 'btn btn-outline-danger'
+  end
+
+  def navbar_link_to(title, href, active: nil)
+    active = is_active_link? href, :inclusive if active.nil?
+    active_class = active ? 'active' : ''
+    content_tag :li, class: "nav-item #{active_class}" do
+      link_to title, href, class: 'nav-link'
+    end
+  end
 end
