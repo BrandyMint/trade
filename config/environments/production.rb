0987ac671aa62c3573c965770e2e253c6356d7b5
Danil Pismenny <danil@brandymint.ru>
2017-03-05 21:05:57 +0300

    remove smtp delivery

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,7 @@ Rails.application.configure do
   config.active_job.queue_adapter     = :sidekiq
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.delivery_method = :smtp
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
