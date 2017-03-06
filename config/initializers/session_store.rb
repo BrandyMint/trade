8bf40a212ce489c6defe1ce66ace3d3bb762564e
Danil Pismenny <danil@brandymint.ru>
2017-03-06 23:04:02 +0300

    sesison store in cooke for tests

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,13 +1,15 @@
 # Be sure to restart your server when you modify this file.
 
-# Rails.application.config.session_store :cookie_store, key: '_trade_session'
-
-Rails.application.config.session_store :redis_session_store, {
-  key: 'op_session',
-  serializer: :hybrid,
-  redis: {
-    expire_after: 4.weeks,
-    key_prefix: 'online_prodaja:session:',
-    url: 'redis://localhost:6379/1',
+if Rails.env.test?
+  Rails.application.config.session_store :cookie_store, key: '_trade_session'
+else
+  Rails.application.config.session_store :redis_session_store, {
+    key: 'op_session',
+    serializer: :hybrid,
+    redis: {
+      expire_after: 4.weeks,
+      key_prefix: 'online_prodaja:session:',
+      url: 'redis://localhost:6379/1',
+    }
   }
-}
+end
