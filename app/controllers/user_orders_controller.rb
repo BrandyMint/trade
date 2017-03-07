e16b49437e0d6d89036f9c6068ada18cba582a24
Danil Pismenny <danil@brandymint.ru>
2017-03-07 23:37:05 +0300

    Заказы входящие/исходящие

--- app/controllers/user_orders_controller.rb
+++ app/controllers/user_orders_controller.rb
@@ -5,12 +5,38 @@ class UserOrdersController < ApplicationController
 
   def index
     render locals: {
-      orders: orders_index
+      income_orders_count: income_orders.count,
+      outcome_orders_count: outcome_orders.count,
+      type: type,
+      orders: orders_index.order('orders.id desc').page(params[:page])
     }
   end
 
   private
+
+  def type
+    if params[:type] == 'outcome'
+      'outcome'
+    else
+      'income'
+    end
+  end
+
+  def income_orders
+    Order.
+      includes(good: [:company]).
+      where(goods: { company_id: 1 })
+  end
+
+  def outcome_orders
+    current_user.orders
+  end
+
   def orders_index
-    current_user.orders.order('id desc').page params[:page]
+    if type == 'income'
+      income_orders
+    else
+      outcome_orders
+    end
   end
 end
