d1e770cf70bc094557e25a6bc9dd36a9a99af94b
Danil Pismenny <danil@brandymint.ru>
2017-03-09 12:17:27 +0300

    fix tests

--- test/controllers/page_controller_test.rb
+++ test/controllers/page_controller_test.rb
@@ -2,8 +2,7 @@ require 'test_helper'
 
 class PageControllerTest < ActionDispatch::IntegrationTest
   test "should get show" do
-    get page_show_url
+    get page_path pages(:one)
     assert_response :success
   end
-
 end
