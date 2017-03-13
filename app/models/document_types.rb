7c39a8ae8f4b08054a74565866ac7047f5c50b4f
Danil Pismenny <danil@brandymint.ru>
2017-03-13 15:34:03 +0300

    Добавил новые типы документов для загрузки (ИНН, ОГРН, Паспорт)

--- app/models/document_types.rb
+++ app/models/document_types.rb
@@ -1,8 +1,14 @@
 DocumentTypesUstav = 'ustav'
 DocumentTypesReshenie = 'reshenie'
 DocumentTypesPrikaz = 'prikaz'
+DocumentTypesInn = 'inn'
+DocumentTypesPassport = 'passport'
+DocumentTypesOgrn = 'ogrn'
 DocumentTypes = [
   OpenStruct.new(title: 'Устав', key: DocumentTypesUstav),
   OpenStruct.new(title: 'Решение о назначении руководителя', key: DocumentTypesReshenie),
   OpenStruct.new(title: 'Приказ о назначении руководителя', key: DocumentTypesPrikaz),
+  OpenStruct.new(title: 'ИНН', key: DocumentTypesInn),
+  OpenStruct.new(title: 'ОГРН', key: DocumentTypesOgrn),
+  OpenStruct.new(title: 'Паспорт руководителя', key: DocumentTypesPassport)
 ]
